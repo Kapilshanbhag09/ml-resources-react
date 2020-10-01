e17b873ab394595109783086192758c3b036beb2
Kapilshanbhag09 <shanbhag.kapil09@gmail.com>
2020-10-01 10:57:48 +0530

    Added books in display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -9,7 +9,7 @@ const Display = () => {
       </h1>
 
       <dl className="json">
-        {resources.map((resource, index) => {
+        {(resources.courses).map((resource, index) => {
           return (
             <div className="bxstyle" key={resource.id}>
               <dt>
@@ -33,6 +33,33 @@ const Display = () => {
           );
         })}
       </dl>
+      <h1><span>Some of the Books</span></h1>
+      <div className="bookslist">
+          {(resources.books).map((book, index) => {
+      return (
+        <div className="booklist" key={book.id}>
+            <span className="title" role="img" aria-label={book.title}>
+              {book.title}
+            </span>
+      <span className="books_Author">{book.Author}</span>
+            <br />
+            <br/>
+            <img src={book.frontcoverimglink}/>
+            <br/>
+            <br/>
+            <a
+              href={book.googlebooks}
+              className="anchor"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Go to the google books-{">"}
+            </a>
+        </div>
+      );
+    })}
+        </div>
+
     </div>
   );
 };
